f28069e8661c9f5a01df2f2d25806a8fb1076624
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-02-13 22:45:06 +0530

    SessionFactoryConfig created...

--- src/main/java/lk/ijse/hibernate_crud/util/SessionFactoryConfig.java
+++ src/main/java/lk/ijse/hibernate_crud/util/SessionFactoryConfig.java
@@ -1,4 +1,27 @@
 package lk.ijse.hibernate_crud.util;
 
+import lk.ijse.hibernate_crud.entity.Customer;
+import org.hibernate.*;
+import org.hibernate.boot.*;
+import org.hibernate.boot.registry.*;
+
 public class SessionFactoryConfig {
+
+    private static SessionFactoryConfig factoryConfig;
+
+    private final SessionFactory sessionFactory;
+
+    private SessionFactoryConfig(){
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+        Metadata metadata = new MetadataSources(serviceRegistry).addAnnotatedClass(Customer.class).getMetadataBuilder().build();
+        sessionFactory = metadata.buildSessionFactory();
+    }
+
+    public static SessionFactoryConfig getInstance(){
+        return (null == factoryConfig) ? factoryConfig = new SessionFactoryConfig() : factoryConfig;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
