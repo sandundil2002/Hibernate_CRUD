5651d9d07a1d7a2006402e7e22d95433fb8087d0
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-02-13 21:58:27 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/hibernate_crud/controller/HelloController.java
@@ -0,0 +1,7 @@
+package lk.ijse.hibernate_crud.controller;
+
+import org.hibernate.*;
+
+
+public class HelloController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module lk.ijse.hibernate_crud {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.hibernate.orm.core;
+
+
+    opens lk.ijse.hibernate_crud to javafx.fxml;
+    exports lk.ijse.hibernate_crud;
+    exports lk.ijse.hibernate_crud.controller;
+    opens lk.ijse.hibernate_crud.controller to javafx.fxml;
+}
