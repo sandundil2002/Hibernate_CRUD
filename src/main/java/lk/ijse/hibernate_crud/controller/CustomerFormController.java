759bf8cd3b7ce56cbf43580f1eab1a47215d441b
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-02-14 11:04:57 +0530

    ui updated...

--- src/main/java/lk/ijse/hibernate_crud/controller/CustomerFormController.java
+++ src/main/java/lk/ijse/hibernate_crud/controller/CustomerFormController.java
@@ -1,10 +1,24 @@
 package lk.ijse.hibernate_crud.controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class CustomerFormController {
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
 
     @FXML
     private TableColumn<?, ?> colAddress;
@@ -36,6 +50,11 @@ public class CustomerFormController {
     @FXML
     private TextField txtMobile;
 
+    public void initialize(){
+        lblDate.setText(LocalDate.now().toString());
+        updateRealTime(lblTime);
+    }
+
     @FXML
     private void btnClearOnAction() {
 
@@ -57,5 +76,19 @@ public class CustomerFormController {
 
     @FXML
     private void btnSearchOnAction() {
+    }
+
+    private void updateRealTime(Label label) {
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            String currentTime = LocalDateTime.now().format(timeFormatter);
+            Platform.runLater(() -> label.setText(currentTime));
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    @FXML
+    private void btnBackOnAction() {
+        System.exit(0);
     }
 }
